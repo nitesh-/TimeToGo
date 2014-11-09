4778c1dc2a3a434f5f99b2418a3bd6794e076f5c
nitzzz60 <nitesh@morajkar.com>
2014-11-09 21:07:38 +0530

    Added some documentation

--- src/com/nitesh/timetogo/TimeToGo.java
+++ src/com/nitesh/timetogo/TimeToGo.java
@@ -1,11 +1,42 @@
+/**
+ * 
+ * Copyright (c) 2014 Nitesh Morajkar
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. 
+ * 
+ */
 package com.nitesh.timetogo;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * 
+ * @author Nitesh Morajkar
+ * 
+ * TimeToGo class converts date(YYYY-mm-dd)  to 'time to go' i.e 10 days to go format 
+ */
 public class TimeToGo {
 	
+	/**
+	 * 
+	 * @param targetDate - Provide the date you want to convert into 'time to go' format
+	 * @return String - Returns date in 'time to go' format
+	 */
 	public static String getTimeLeft(String targetDate) {
 		//String targetDate = "2014-11-11";
 		String targetDateArray[] = targetDate.split("-");
@@ -82,7 +113,7 @@ public class TimeToGo {
 		}
 		
 		if(!str.equals("")) {
-			str += "left";
+			str += "to go";
 		}
 		if(arg[0].equals("current")) {
 			str = "Today";
